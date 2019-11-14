ca7385ee281fb2de4a00f4f7ed7040118562d1ca
Doru Paraschiv <doru.paraschiv@vgw.co>
2019-11-14 22:08:20 +0200

    e2e test updated

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { async } from 'rxjs/internal/scheduler/async';
+import { getConnection } from 'typeorm';
+import { NestApplication } from '@nestjs/core';
 
 describe('AppController (e2e)', () => {
-  let app;
+  let app: NestApplication;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -14,10 +17,15 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  afterAll(async () => {
+    await app.close();
+  });
+
   it('/ (GET)', () => {
+    let expected = [{"id":1,"name":"Kiki","typeId":1,"age":1,"breed":"labrador"}];
     return request(app.getHttpServer())
-      .get('/')
+      .get('/animals')
       .expect(200)
-      .expect('Hello World!');
+      .expect(expected);
   });
 });
